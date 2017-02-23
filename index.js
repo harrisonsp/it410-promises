5946c148964f20cdba6e14f8933f5cde774b5043
harrisonsp <harrisonpusey@Harrisons-Air-010010105178.app.byu.edu>
2017-02-22 18:32:56 -0700

    positinon

--- index.js
+++ index.js
@@ -26,9 +26,10 @@ exports.readDir = function(dirPath){
 }
 
 exports.readDirFiles = function(dirPath){
+    var retArray = []
+
     return exports.readDir(dirPath)
         .then( function (somefiles) {
-           var retArray = []
             for( var i; i < somefiles.length; i++){
                 retArray.push(exports.readFile(exports.resolvedPath(dirPath, somefiles[i])));
             }
